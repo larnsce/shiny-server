04ddc51312301ee65c23d55a62d68a9879850130
larnsce <lars@lse.de>
2019-03-06 14:24:53 +0100

    - add unique function

--- kpi/server.R
+++ kpi/server.R
@@ -14,29 +14,32 @@ library(lubridate)
 
 # load data ---------------------------------------------------------------
 
-## for local development
+## for local development#
 # kpi_db <- read_csv(file = here::here("kpi/data/daily_updated_database.csv")) %>% unique()
 
 ## for SQL deployment
 
-# con_mydb <- DBI::dbConnect(RMySQL::MySQL(),
-#                            user = Sys.getenv("userid"),
-#                            password = Sys.getenv("pwd1"), #rstudioapi::askForPassword("Database password"),
-#                            host = '185.178.195.36',
-#                            port = 3306,
-#                            
-#                            ## Selected database to be entered through terminal
-#                            
-#                            dbname = 'BMC_prod')
-# 
-# kpi_db <- dbReadTable(conn = con_mydb, name = "BMC_KPI_DB")
-# 
-# dbDisconnect(conn = con_mydb)
-# 
+#con_mydb <- DBI::dbConnect(RMySQL::MySQL(),
+#                           user = Sys.getenv("userid"),
+#                           password = Sys.getenv("pwd1"), #rstudioapi::askForPassword("Database password"),
+#                           host = '185.178.195.36',
+#                           port = 3306,
+#                           
+#                           ## Selected database to be entered through terminal
+#                           
+#                           dbname = 'BMC_prod')
+#
+#kpi_db <- dbReadTable(conn = con_mydb, name = "BMC_KPI_DB")
+#
+#kpi_db %>% write_csv("kpi/data/test_data_locally.csv")
+#
+#dbDisconnect(conn = con_mydb)
+
+# kpi_db <- read_csv(file = here::here("kpi/data/test_data_locally.csv")) %>% unique()
 
 ## for online deployment
 
-kpi_db <- read_csv(file = "data/bmc_kpi_score_database.csv")
+kpi_db <- read_csv(file = "data/bmc_kpi_score_database.csv") %>% unique()
 
 ### filter out the firmware version
 
